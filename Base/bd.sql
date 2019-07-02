b40ab823a0e51f2139ed89820794d638397db492
Alan Santos <alanssantos32@gmail.com>
2019-07-02 12:35:57 -0300

    Update bd.sql

--- Base/bd.sql
+++ Base/bd.sql
@@ -3,16 +3,19 @@ CREATE DATABASE "ds2Mail";
 CREATE TABLE "user" (
     "code" serial,
     "email" varchar(100) NOT NULL,
-    "password" varchar(1000) NOT NULL
+    "password" varchar(1000) NOT NULL,
+    CONSTRAINT "userPK" PRIMARY KEY ("code")
 );
 CREATE TABLE "public" (
     "code" serial,
     "email" varchar(100) NOT NULL,
-    "message" varchar NOT NULL
+    "message" varchar NOT NULL,
+    CONSTRAINT "publicPK" PRIMARY KEY ("code")
 );
 CREATE TABLE "subscribe" (
     "code" serial,
-    "email" varchar(100) NOT NULL
+    "email" varchar(100) NOT NULL,
+    CONSTRAINT "subsPK" PRIMARY KEY ("code")
 );
 
 INSERT INTO "user" (email, password) values ('admin@gmail.com', 'admin');
