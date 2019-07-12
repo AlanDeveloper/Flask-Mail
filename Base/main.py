f2d9245f4614d99645b480a861b311f56f274af1
Alan Santos <alanssantos32@gmail.com>
2019-07-12 10:48:47 -0300

    Add files via upload

---
+++ Base/main.py
@@ -0,0 +1,73 @@
+from flask import Flask, flash, render_template, request, redirect, url_for, session
+from flask_mail import Mail, Message
+from threading import Thread
+from server import ServerDAO
+
+app = Flask(__name__, template_folder='template')
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/register', methods=['POST'])
+def reg():
+    ServerDAO().subscribe(request.form['email'])
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		result = ServerDAO().login(request.form['email'], request.form['password'])
+
+		if result == None:
+			flash('Dados incorretos')
+			return redirect(url_for('login'))
+		else:
+			session['username'] = request.form['email'].split('@')[0]
+			session['password'] = request.form['password'] 
+			return redirect(url_for('feed'))
+	
+	if 'username' not in session:
+		return render_template('login.html')
+	else:
+		return redirect(url_for('feed'))
+
+@app.route('/loggout')
+def loggout():
+	session.clear()
+	return redirect(url_for('index'))
+
+def send_mail(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+@app.route('/feed', methods=['GET', 'POST'])
+def feed():
+	if request.method == 'POST':
+		result = ServerDAO().send(session['username'], request.form['message'])
+		if result:
+			recip = ServerDAO().search()
+
+			msg = Message()
+			msg.subject = 'Há uma nova publicação na página!'
+			msg.recipients = recip
+			msg.html = render_template('notification.html')
+
+			thr = Thread(target=send_mail, args=[app, msg])
+			thr.start()
+
+		return redirect(url_for('feed'))
+	return render_template('feed.html')
+
+if __name__ == '__main__':
+	app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+	app.config['MAIL_PORT'] = 465
+	app.config['MAIL_USERNAME'] = 'genicandogenerico@gmail.com'
+	app.config['MAIL_PASSWORD'] = 'adm@123abc'
+	app.config['MAIL_DEFAULT_SENDER'] = 'genicandogenerico@gmail.com'
+	app.config['MAIL_USE_TLS'] = False
+	app.config['MAIL_USE_SSL'] = True
+	mail = Mail(app)
+
+	app.run(debug=True)
